0e1f332512f7ab30b10bca68af0f14df7d672669
Luiz <luizpfern14@gmail.com>
2025-07-06 15:37:38 -0300

    assets

---
+++ assets/world/itens.tsx
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="itens" tilewidth="2016" tileheight="64" tilecount="62" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="miscellaneous sprites/antidote_potion.png" width="8" height="10"/>
+ </tile>
+ <tile id="1">
+  <image source="miscellaneous sprites/apple_item.png" width="8" height="8"/>
+ </tile>
+ <tile id="2">
+  <image source="miscellaneous sprites/arrow_plate_left.png" width="16" height="16"/>
+ </tile>
+ <tile id="3">
+  <image source="miscellaneous sprites/arrow_plate_right.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="miscellaneous sprites/bigflowers_props.png" width="16" height="8"/>
+ </tile>
+ <tile id="5">
+  <image source="miscellaneous sprites/bomb_on_ground_anim.gif" width="8" height="8"/>
+ </tile>
+ <tile id="6">
+  <image source="miscellaneous sprites/bomb_on_ground_anim_strip_8.png" width="64" height="8"/>
+ </tile>
+ <tile id="7">
+  <image source="miscellaneous sprites/bomb_thrown_anim.gif" width="8" height="8"/>
+ </tile>
+ <tile id="8">
+  <image source="miscellaneous sprites/bomb_thrown_anim_strip_3.png" width="24" height="8"/>
+ </tile>
+ <tile id="9">
+  <image source="miscellaneous sprites/buttom.png" width="16" height="4"/>
+ </tile>
+ <tile id="10">
+  <image source="miscellaneous sprites/buttom_pressed.png" width="16" height="4"/>
+ </tile>
+ <tile id="11">
+  <image source="miscellaneous sprites/coin_anim.gif" width="8" height="8"/>
+ </tile>
+ <tile id="12">
+  <image source="miscellaneous sprites/coin_anim_strip_6.png" width="48" height="8"/>
+ </tile>
+ <tile id="13">
+  <image source="miscellaneous sprites/coin_pickup_anim.gif" width="8" height="16"/>
+ </tile>
+ <tile id="14">
+  <image source="miscellaneous sprites/coin_pickup_anim_strip_6.png" width="48" height="16"/>
+ </tile>
+ <tile id="15">
+  <image source="miscellaneous sprites/door.png" width="24" height="24"/>
+ </tile>
+ <tile id="16">
+  <image source="miscellaneous sprites/drygrass_props.png" width="16" height="8"/>
+ </tile>
+ <tile id="17">
+  <image source="miscellaneous sprites/explosion_anim.gif" width="32" height="32"/>
+ </tile>
+ <tile id="18">
+  <image source="miscellaneous sprites/explosion_anim_strip_10.png" width="320" height="32"/>
+ </tile>
+ <tile id="19">
+  <image source="miscellaneous sprites/flowers_props.png" width="16" height="8"/>
+ </tile>
+ <tile id="20">
+  <image source="miscellaneous sprites/grass_props.png" width="16" height="8"/>
+ </tile>
+ <tile id="21">
+  <image source="miscellaneous sprites/health_potion.png" width="8" height="10"/>
+ </tile>
+ <tile id="22">
+  <image source="miscellaneous sprites/lever_turned_left.png" width="16" height="16"/>
+ </tile>
+ <tile id="23">
+  <image source="miscellaneous sprites/lever_turned_right.png" width="16" height="16"/>
+ </tile>
+ <tile id="24">
+  <image source="miscellaneous sprites/loot_box_anim.gif" width="16" height="8"/>
+ </tile>
+ <tile id="25">
+  <image source="miscellaneous sprites/loot_box_anim_opening.gif" width="16" height="10"/>
+ </tile>
+ <tile id="26">
+  <image source="miscellaneous sprites/loot_box_anim_opening_strip_6.png" width="96" height="10"/>
+ </tile>
+ <tile id="27">
+  <image source="miscellaneous sprites/loot_box_anim_strip_12.png" width="192" height="8"/>
+ </tile>
+ <tile id="28">
+  <image source="miscellaneous sprites/loot_box_open.png" width="16" height="10"/>
+ </tile>
+ <tile id="29">
+  <image source="miscellaneous sprites/meat_item.png" width="8" height="8"/>
+ </tile>
+ <tile id="30">
+  <image source="miscellaneous sprites/orb.png" width="8" height="8"/>
+ </tile>
+ <tile id="31">
+  <image source="miscellaneous sprites/orb_anim.gif" width="8" height="8"/>
+ </tile>
+ <tile id="32">
+  <image source="miscellaneous sprites/orb_anim_strip_6.png" width="48" height="8"/>
+ </tile>
+ <tile id="33">
+  <image source="miscellaneous sprites/orb_collected_anim.gif" width="8" height="8"/>
+ </tile>
+ <tile id="34">
+  <image source="miscellaneous sprites/orb_collected_anim_strip_5.png" width="40" height="8"/>
+ </tile>
+ <tile id="35">
+  <image source="miscellaneous sprites/root_props.png" width="16" height="8"/>
+ </tile>
+ <tile id="36">
+  <image source="miscellaneous sprites/save_point_anim.gif" width="12" height="20"/>
+ </tile>
+ <tile id="37">
+  <image source="miscellaneous sprites/save_point_anim_strip_9.png" width="108" height="20"/>
+ </tile>
+ <tile id="38">
+  <image source="miscellaneous sprites/save_point_saving_anim.gif" width="12" height="20"/>
+ </tile>
+ <tile id="39">
+  <image source="miscellaneous sprites/save_point_saving_anim_strip_3.png" width="36" height="20"/>
+ </tile>
+ <tile id="40">
+  <image source="miscellaneous sprites/spikes.png" width="16" height="16"/>
+ </tile>
+ <tile id="41">
+  <image source="miscellaneous sprites/spikes_trap.png" width="16" height="16"/>
+ </tile>
+ <tile id="42">
+  <image source="miscellaneous sprites/spikes_trap_hit_wall_dust_effect.gif" width="16" height="4"/>
+ </tile>
+ <tile id="43">
+  <image source="miscellaneous sprites/spikes_trap_hit_wall_dust_effect_strip_3.png" width="48" height="4"/>
+ </tile>
+ <tile id="44">
+  <image source="miscellaneous sprites/stone.png" width="16" height="16"/>
+ </tile>
+ <tile id="45">
+  <image source="miscellaneous sprites/strange_door_closed_anim.gif" width="16" height="48"/>
+ </tile>
+ <tile id="46">
+  <image source="miscellaneous sprites/strange_door_closed_anim_strip_10.png" width="160" height="48"/>
+ </tile>
+ <tile id="47">
+  <image source="miscellaneous sprites/strange_door_opening_anim.gif" width="16" height="48"/>
+ </tile>
+ <tile id="48">
+  <image source="miscellaneous sprites/strange_door_opening_anim_strip_14.png" width="224" height="48"/>
+ </tile>
+ <tile id="49">
+  <image source="miscellaneous sprites/tiki_torch_props.gif" width="8" height="24"/>
+ </tile>
+ <tile id="50">
+  <image source="miscellaneous sprites/tiki_torch_props_strip_12.png" width="96" height="24"/>
+ </tile>
+ <tile id="51">
+  <image source="miscellaneous sprites/trap_spikes_anim.gif" width="16" height="16"/>
+ </tile>
+ <tile id="52">
+  <image source="miscellaneous sprites/trap_spikes_anim_strip_7.png" width="112" height="16"/>
+ </tile>
+ <tile id="53">
+  <image source="miscellaneous sprites/trap_suspended_anim.gif" width="112" height="64"/>
+ </tile>
+ <tile id="54">
+  <image source="miscellaneous sprites/trap_suspended_anim_strip_18.png" width="2016" height="64"/>
+ </tile>
+ <tile id="55">
+  <image source="miscellaneous sprites/vase.png" width="16" height="16"/>
+ </tile>
+ <tile id="56">
+  <image source="miscellaneous sprites/vase_breaking_anim.gif" width="16" height="16"/>
+ </tile>
+ <tile id="57">
+  <image source="miscellaneous sprites/vase_breaking_anim_strip_5.png" width="80" height="16"/>
+ </tile>
+ <tile id="58">
+  <image source="miscellaneous sprites/wood_slab_left.png" width="16" height="8"/>
+ </tile>
+ <tile id="59">
+  <image source="miscellaneous sprites/wood_slab_middle.png" width="16" height="8"/>
+ </tile>
+ <tile id="60">
+  <image source="miscellaneous sprites/wood_slab_right.png" width="16" height="8"/>
+ </tile>
+ <tile id="61">
+  <image source="miscellaneous sprites/wooden_plate.png" width="16" height="16"/>
+ </tile>
+</tileset>
